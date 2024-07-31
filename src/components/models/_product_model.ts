32bdbc7c54338cc37dca84f8564849f3e28128f3
chandankumar1307 <chandan.kumar@elitemindz.co>
2024-07-31 18:34:59 +0530

    added interface

--- src/components/models/_product_model.ts
+++ src/components/models/_product_model.ts
@@ -12,3 +12,19 @@ export interface ProductProps {
   category_id: number;
   isDeleted: boolean;
 }
+
+export interface CategoryLayout {
+  category_id: number;
+  category_name: string;
+  category_layout: string;
+  category_banner1: string;
+  productList: categoryProduct[];
+}
+
+export interface categoryProduct {
+  product_id: number;
+  product_name: string;
+  product_image: string;
+  category_id: number;
+  product_description: string;
+}
