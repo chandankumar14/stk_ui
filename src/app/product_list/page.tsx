f9157b23973b128b025d224506fd56db69fc2eda
Developer <chandankumar75707@gmail.com>
2024-10-10 20:50:26 +0530

    addded new section and style

--- src/app/product_list/page.tsx
+++ src/app/product_list/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "@/Redux/cartSlice"
@@ -8,9 +7,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { GetProductList } from "../services/_home_page_service";
 import { Product } from "@/components/models/_product_model";
+import MostSellingProduct from "@/components/carousel/_most_selling";
 export default function ProductList() {
-  const router = useRouter();
-  const dispatch = useDispatch();
+ const dispatch = useDispatch();
   const [products, setProducts] = useState<Product[]>([]);
   const [error, setError] = useState<string | null>();
   useEffect(() => {
@@ -44,15 +43,16 @@ export default function ProductList() {
           </div>
         </div>
       </div>
+
       <div className="ml-2 bg-white rounded-none">
-        <div className="font-[sans-serif] bg-gray-100">
+        <div className="font-[sans-serif] ">
           <div className="p-4 mx-auto lg:max-w-7xl sm:max-w-full">
             <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-xl:gap-4 gap-6">
               {products.map((product) => (
                 <div key={product.id}  className="bg-pink-100 rounded-2xl p-5 cursor-pointer hover:-translate-y-2 transition-all relative">
                   <div
-                    className="bg-gray-100 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer absolute top-4 right-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16px" className="fill-gray-800 inline-block" viewBox="0 0 64 64">
+                    className=" w-10 h-10 flex items-center justify-center rounded-full cursor-pointer absolute top-4 right-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16px" className="fill-red-600 inline-block" viewBox="0 0 64 64">
                       <path
                         d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
                         data-original="#000000"></path>
@@ -91,8 +91,8 @@ export default function ProductList() {
           </div>
         </div>
       </div>
-
-
+      {/** Most Selling Product list  */}
+      <MostSellingProduct />
     </>
   );
 }
